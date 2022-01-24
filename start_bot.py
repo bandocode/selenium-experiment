1222839d17850434838e219755bd3b781bec55fc
apetheory <66187780+apetheory@users.noreply.github.com>
2022-01-24 09:34:31 +0000

    Update start_bot.py

--- start_bot.py
+++ start_bot.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# author: @frigarui
 
 from selenium import webdriver
 from functools import cache
@@ -14,12 +13,14 @@ def main():
 	parser.add_argument('-p', '--password', type=str, required=True, help='The password of your account on smartrevise.online')
 	parser.add_argument('-l', '--link', type=str, required=True, help='The number at the end of the link where you answer questions')
 	parser.add_argument('-H', '--headless', type=bool, help='Run in headless mode? True/False')
+	parser.add_argument('-L', '--limit', type=int, help='How many questions to answer?')
 	args = parser.parse_args()
 
 	# Attribute the arguments to corresponding variables
 	browser.cred_email = args.email
 	browser.cred_password = args.password
 	bot.answering_page = f"https://smartrevise.online/student/revise/Question/{args.link}"
+	bot.limit = args.limit
 
 	if args.headless == True:
 		browser.headless = True
