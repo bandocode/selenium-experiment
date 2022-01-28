2b2422416cab4b0ca0fa67d8ae71f3c6f5895c59
apetheory <christian4226@outlook.com>
2022-01-28 21:37:12 +0000

    delete global_vars.py

--- global_vars.py
+++
@@ -1,6 +0,0 @@
-from browser import Browser
-from answering import answer_object
-
-# Create the objects in a separate file so they can be accessed by other files more easily.
-browser = Browser()
-bot = answer_object()
